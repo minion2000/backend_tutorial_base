2acf264ed338b2df9789d7c667870bca64d58c13
Stitch <20micrometer@gmail.com>
2025-11-29 23:22:58 +0900

    Getのルート

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// API v1
+Route::prefix('v1')->group(function () {
+    // コメント一覧取得（認証不要）
+    Route::get('/articles/{article}/comments', [CommentController::class, 'index']);
+});
